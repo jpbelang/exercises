25d4368d819389864c2097bcb76242c9e6a95e55
Jean-Philippe Belanger <jean-philippe.belanger@ericsson.com>
2015-11-23 08:27:30 -0500

    Just basic classes
    Change-Id: Ib9e45d93770544510e48eed52cf3627bb56daf62

---
+++ io/src/main/java/ca/eloas/exercises/io/Reader.java
@@ -0,0 +1,8 @@
+package ca.eloas.exercises.io;
+
+/**
+ * Created by ebeljea on 11/23/15.
+ * Copyright Ericsson.
+ */
+public class Reader {
+}
---
+++ io/src/test/java/ca/eloas/exercises/io/ReaderTest.java
@@ -0,0 +1,11 @@
+package ca.eloas.exercises.io;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by ebeljea on 11/23/15.
+ * Copyright Ericsson.
+ */
+public class ReaderTest {
+
+}
